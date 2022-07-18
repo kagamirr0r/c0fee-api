b7058ea1122c93e36aaf8ff8176fe3688ee9161c
Kagamirr0r <a7112069@gmail.com>
2022-07-19 00:06:45 +0900

    device外し

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,5 @@
 class TopPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  # skip_before_action :authenticate_user!, only: [:index]
   def index
     @beans = Bean.with_translations.where(bean_translations: { locale: I18n.locale }).first(12)
     @recipes = Recipe.joins(bean: :translations).where(bean_translations: { locale: I18n.locale }).first(12)
